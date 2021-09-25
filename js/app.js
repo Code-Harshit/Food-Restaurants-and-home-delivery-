45059213fe1413a55a78d936ba8df93b39600f29
Harshit Madenkar <harshitmandekar@gmail.com>
2021-09-26 05:14:59 +0530

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $(".food-slider").slick({
+        autoplay:true,
+        slidesToShow:3,
+        slidesToScroll:1,
+        prevArrow:".prev-btn",
+        nextArrow:".next-btn",
+        
+    });
+
+});
